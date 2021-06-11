b181fc60f4bed1da39bc8b921c8360440c410391
yangyunhui454545 <diddbsgml24@duksung.ac.kr>
2021-06-11 17:50:44 +0900

    Initial commit

---
+++ src/main/java/com/lightningboys/uhchinseo/domain/AZGame.java
@@ -0,0 +1,28 @@
+package com.lightningboys.uhchinseo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "az_game")
+@Getter @Setter
+public class AZGame {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "az_id")
+    private Long id;
+
+    @Column(name = "az_question")
+    private String question;
+
+    @Column(name="az_selection")
+    private String selection;
+
+    @Column(name = "az_answer")
+    private boolean answer;
+
+    @Column(name = "az_category")
+    private Character category;
+
+}
