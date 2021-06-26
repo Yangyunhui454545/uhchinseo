deaf8b0f7c39752efa4b29da9b1bd117cf87bc78
herineeda <ihyelin185@gmail.com>
2021-06-26 16:45:59 +0900

    balancegame first commit

--- src/main/java/com/lightningboys/uhchinseo/Service/BalanceGameService.java
+++ src/main/java/com/lightningboys/uhchinseo/Service/BalanceGameService.java
@@ -1,4 +1,16 @@
 package com.lightningboys.uhchinseo.Service;
 
+import com.lightningboys.uhchinseo.Domain.BalanceGame;
+import com.lightningboys.uhchinseo.Repository.BalanceGameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class BalanceGameService {
 }
