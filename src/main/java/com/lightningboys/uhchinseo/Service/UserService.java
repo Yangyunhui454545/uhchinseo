7fdc109c87e485b649086d695853f776c00627b2
yangyunhui454545 <codingbutter@naver.com>
2021-06-26 20:06:21 +0900

    user id 확인용 로직

--- src/main/java/com/lightningboys/uhchinseo/Service/UserService.java
+++ src/main/java/com/lightningboys/uhchinseo/Service/UserService.java
@@ -1,4 +1,21 @@
 package com.lightningboys.uhchinseo.Service;
 
+import com.lightningboys.uhchinseo.Domain.User;
+import com.lightningboys.uhchinseo.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public Optional<User> findById(Long id){
+        Optional<User> user= userRepository.findById(id);
+        return user;
+    }
 }
